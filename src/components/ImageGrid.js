e8a90bfd26c3c8f7b166ec795ab61d166d282471
Timo Jaaskelainen <timojaask@gmail.com>
2018-03-21 23:02:05 -0300

    Add a rudimentary image switcher

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import Img from "gatsby-image";
 import styles from "./ImageGrid.module.css";
 
-export const ImageGrid = ({ photos }) => renderImages(photos)
+export class ImageGrid extends Component {
+  constructor(props) {
+    super();
+    this.state = { photos: props.photos }
+  }
+
+  onPhotoSelected(photoIndex) {
+    // Take photo at index and swap it with photo at index 0
+    console.log('swapping photos: 0 and ' + photoIndex);
+    let photos = this.state.photos;
+    const firstPhoto = photos[0];
+    photos[0] = photos[photoIndex];
+    photos[photoIndex] = firstPhoto;
+    this.setState({ photos })
+  }
+
+  render() {
+    return renderImages({
+      photos: this.state.photos,
+      onPhotoSelected: idx => { this.onPhotoSelected(idx) }
+    })
+  }
+}
 
 const styleForIndex = idx => {
   switch (idx) {
@@ -13,21 +35,21 @@ const styleForIndex = idx => {
   }
 }
 
-const renderImages = photos =>
+const renderImages = ({ photos, onPhotoSelected }) =>
   photos.length > 1 ?
-    <MultipleImages largePhoto={photos[0]} thumbnails={photos.slice(1)} /> :
+    <MultipleImages largePhoto={photos[0]} thumbnails={photos.slice(1)} onPhotoSelected={onPhotoSelected} /> :
     <OneImage photo={photos[0]} />
 
 const OneImage = ({ photo }) => <Img sizes={photo.sizes} />
 
-const MultipleImages = ({ largePhoto, thumbnails }) =>
+const MultipleImages = ({ largePhoto, thumbnails, onPhotoSelected }) =>
   <div className={styles.container}>
     <div className={styles.largeImage}>
       <Img sizes={largePhoto.sizes} />
     </div>
     {thumbnails.map((photo, idx) =>
-      <div key={idx} className={styleForIndex(idx)}>
+      <button onClick={() => { onPhotoSelected(idx + 1) }} key={idx} className={styleForIndex(idx + 1)}>
         <Img sizes={photo.sizes} />
-      </div>
+      </button>
     )}
   </div>
