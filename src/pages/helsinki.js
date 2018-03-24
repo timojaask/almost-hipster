7ab9664a426dd3376930bb3281ce00c8d4325b35
Timo Jaaskelainen <timojaask@gmail.com>
2018-03-23 23:53:26 -0300

    Remove old comment

--- src/pages/helsinki.js
+++ src/pages/helsinki.js
@@ -5,56 +5,6 @@ import { FootnoteLink, FootnoteList } from "../components/Footnotes"
 import { ImageGrid } from "../components/ImageGrid"
 import styles from "./blogpost.module.css";
 
-/*
-
-Need photos from:
-- Artisan Café
-- Roasberg
-- Fratello
-- Liberty or Death
-- Books & Antiques
-- Pastor
-- Kaisla
-- Block By Dylan
-- Momotoko
-- Samrat
-- Piece'n'Love
-- Baskeri & Basso
-- Holiday
-
-Coffee:
-Third-wave coffee shops is definitely not one of the city's strongest points, but it's got a few great options. Generally, Finnish coffee culture is 99% filter coffee made at home or at work. This is what I grew up with and I hated it. However, places like Artisan Café totally make up for all the bad coffee in the country.
-
-Artisan Café
-Jani is the owner and one of the most devoted coffee makers I've had a pleasure to meet. He once told me, that for each drink that he makes, he wants it to be perfect. So he'll redo it as many times as necessary until he gets every detail just right before serving it to a customer. Devotion to the art speaks for itself once you get to taste his work. Artisan is my favorite.
-
-Johan & Nystrom
-This super cozy coffee shop is located on a beautiful Katajanokka island. Random-looking, but at the same time very stylish decor and brick walls create a wonderful relaxing atmosphere, and a unique place.
-
-Fratello
-
-Roasberg
-
-Bars:
-I'm a sucker for a good cocktail, and I think Helsinki is the city that really spoiled me in that regard. The four cocktail bars listed below are among the best I've been to in my life, competing with cities like New York, Tel Aviv, Barcelona, and Lisbon.
-
-Liberty or Death -- 
-Trillby & Chadwick
-Books & Antiques
-Pastor
-Kaisla
-
-Food:
-There are so many great places to eat in Helsinki, that it would make for a very long blog post if I tried to cover it all. And I'd probably fail at that. So here are a few of my personal favorites.
-
-Block By Dylan
-Momotoko
-Samrat
-Piece'n'Love
-Baskeri & Basso
-Holiday
-
-*/
 export default ({ data }) => (
   <div>
     <PostHeader coverImage={data.cover} name="Helsinki" date={"Oct 2016"} />
